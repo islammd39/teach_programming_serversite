b04dc2e7fb6567af67a33627698d4c9309d9a10c
islammd39e <islammdrahat308@gmail.comm>
2022-10-25 17:30:10 +0600

    send to data in categoty

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+const categories = require('./data/categories.json')
+
 app.get('/', (req, res) => {
   res.send('Hello in World!')
+})
+app.get('/programming-languages', (req, res) => {
+  res.send(categories) 
 })
 
 app.listen(port, () => {
